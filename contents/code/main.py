736056769668877bc882a3309f1e4e27bd13c863
Michael Mair-Keimberger (asterix) <m.mairkeimberger@gmail.com>
2015-08-06 17:19:42 +0200

    implement python notify

--- contents/code/main.py
+++ contents/code/main.py
@@ -3,8 +3,24 @@ from PyKDE4.plasma import Plasma
 from PyKDE4.kdeui import KIcon, KMessageBox
 import fnmatch
 import os
+import dbus
 from subprocess import call
 from subprocess import check_output
+
+# notify function -> this is a python notify version
+# maybe switch to a proper kde notify version, since
+# this is for kde only anyway
+def notify(summary, body='', app_name='', app_icon='', timeout=5000, actions=[], hints=[], replaces_id=0):
+    _bus_name = 'org.freedesktop.Notifications'
+    _object_path = '/org/freedesktop/Notifications'
+    _interface_name = _bus_name
+
+    session_bus = dbus.SessionBus()
+    obj = session_bus.get_object(_bus_name, _object_path)
+    interface = dbus.Interface(obj, _interface_name)
+    interface.Notify(app_name, replaces_id, app_icon,
+    summary, body, actions, hints, timeout)
+
 
 class MsgBoxRunner(plasmascript.Runner):
 
@@ -48,7 +64,8 @@ class MsgBoxRunner(plasmascript.Runner):
         limit = 0
         for i in keys:
             limit += 1
-            if limit > 16:
+            # only show the first 15 matches
+            if limit > 15:
                 break
             pw=str(i).replace('/home/michael/.password-store/','').replace('.gpg','')
             m.setData(pw)
@@ -60,8 +77,14 @@ class MsgBoxRunner(plasmascript.Runner):
         # called by KRunner when the user selects our action,        
         # so lets keep our promise
 
-        #KMessageBox.messageBox(None, KMessageBox.Information, match.data().toString())
-        msg=check_output(["pass","-c","%s" % str(match.data().toString())])
+        # get return code of pass (somehow check_output with pass -c kills krunner)
+        rc=call(["pass","-c","%s" % str(match.data().toString())])
+        # 0 = means password was correct
+        if rc == 0:
+            # text is basically the same like what pass outputs
+            notify(summary="pass", app_icon="dialog-information", body="Copied %s to clipboard. Will clear in 45 seconds." % str(match.data().toString()))
+        else:
+            notify(summary="pass", app_icon="dialog-warning",  body="gpg: decryption failed: No secret key")
 
 
 def CreateRunner(parent):
